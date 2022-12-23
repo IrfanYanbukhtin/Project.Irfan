6ae3d4c53dde39414403a3dbe67938f557fb4bf1
irfan <Asery_valha@DESKTOP-707VBL0>
2022-12-23 10:55:51 +0400

    Добавьте файлы проекта.

---
+++ Project.Irfan/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Project.Irfan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {      
+          int i = 1;
+
+          while (true) 
+               
+          Console.WriteLine(i+=15);      
+                        
+           
+         }
+    }
+}
